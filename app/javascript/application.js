dacfe48327724fceee1c930b0e26ad19dae0309b
Kenta <kenta@Kentas-MacBook-Pro.local>
2025-12-04 11:39:20 +0900

    トップ画面の作成

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,26 @@
-// Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import "./header"
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js";
+
+document.addEventListener("turbo:load", () => {
+  new Swiper('.swiper', {
+    loop: true,
+    pagination: { el: '.swiper-pagination' },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
+
+const cardSwiper = new Swiper(".card-swiper", {
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  freeMode: true,   // ← スムーズに指でスライドできる
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+});
