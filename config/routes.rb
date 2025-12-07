2f68a004e622b3b62bac0e39bc9e5696e48bd070
Kenta <kenta@Kentas-MacBook-Pro.local>
2025-12-07 15:51:59 +0900

    ログイン設定とホーム画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root to: "static_pages#top"
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  root "static_pages#top"
 end
