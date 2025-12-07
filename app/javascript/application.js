989820672f00c1c54a6a4964b151f4774351930a
Kenta <kenta@Kentas-MacBook-Pro.local>
2025-12-07 16:17:05 +0900

    修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,9 +2,15 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 import "./header"
-import { Swiper } from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+import Swiper from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 document.addEventListener("turbo:load", () => {
+
+  // ★ 通常スライダー
   new Swiper('.swiper', {
     loop: true,
     pagination: { el: '.swiper-pagination' },
@@ -13,14 +19,16 @@ document.addEventListener("turbo:load", () => {
       prevEl: '.swiper-button-prev',
     },
   });
-});
 
-const cardSwiper = new Swiper(".card-swiper", {
-  slidesPerView: 'auto',
-  spaceBetween: 16,
-  freeMode: true,   // ← スムーズに指でスライドできる
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true,
-  },
+  // ★ 自動スクロールするカードスライダー
+  new Swiper(".card-swiper", {
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    freeMode: true,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: true,
+    },
+  });
+
 });
